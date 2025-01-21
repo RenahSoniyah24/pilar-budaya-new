44816142a0ed96e7ce270a7ff51fb83346ff8a02
Andrian Aji <andrianaji06@gmail.com>
2025-01-21 18:07:42 +0700

    perubahan detail account

--- src/Views/Admin/Page/Iuran/Detail.js
+++ src/Views/Admin/Page/Iuran/Detail.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { FaWallet, FaCheck } from "react-icons/fa";
-import { getUserDetailService } from '../../../../Services/ServicesAPI';
+import { FaWallet, FaCheck } from 'react-icons/fa';
+import {
+  getUserDetailService,
+  verifikasiService,
+} from '../../../../Services/ServicesAPI';
 import { useParams } from 'react-router-dom';
+import Notification from '../../../../Components/Notification';
 
 import 'antd/dist/reset.css';
 import { Table } from 'antd';
 import qs from 'qs';
-import {  
-  coloumn_detail_iuran,
-} from '../../../../Constant/DataColoumn';
-import {  
-  formatDate,
-  calculateAge
-} from '../../../../../src/Formatter/Text';
+import { coloumn_detail_iuran } from '../../../../Constant/DataColoumn';
+import { formatDate, calculateAge } from '../../../../../src/Formatter/Text';
 
 import Admin from '../../Index';
 
 function IuranDetail(props) {
-  const {identifier}                  = useParams()
-  const [data, setData]               = useState({});
-  const [dataTable, setDataTable]     = useState([]);
-  const [loading, setLoading]         = useState(false);
-  const [isLoading, setIsLoading]     = useState(true);
-  const [dataModal, setDataModal]     = useState({});
+  const { identifier } = useParams();
+  const [data, setData] = useState({});
+  const [dataTable, setDataTable] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataModal, setDataModal] = useState({});
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
@@ -34,17 +33,17 @@ function IuranDetail(props) {
   const fetchData = async () => {
     setLoading(true);
 
-    let response = await getUserDetailService(identifier);
-
+    let response = await getUserDetailService(identifier, 'iuran');
+    console.log(response);
     if (response) {
-      let data = response.data
-      let formatBirth = formatDate(data.birthDate)
-      let age = calculateAge(data.birthDate)
+      let data = response.data;
+      let formatBirth = formatDate(data.birthDate);
+      let age = calculateAge(data.birthDate);
 
       setData({
         ...data,
         birthDate: formatBirth,
-        age: `${age} Tahun`
+        age: `${age} Tahun`,
       });
       setDataTable(response.data.payments);
       setLoading(false);
@@ -80,10 +79,18 @@ function IuranDetail(props) {
       setDataTable([]);
     }
   };
-
+  const handleVerifikasi = async (val) => {
+    setLoading(true);
+    let response = await verifikasiService(identifier, val);
+    if (response) {
+      setLoading(false);
+      Notification.success('Berhasil, ', 'Verifikasi Pembayaran Berhasil');
+      await fetchData();
+    }
+  };
   // handle data modal
-  const handleDataModal= (val) => {
-    setDataModal(val)
+  const handleDataModal = (val) => {
+    setDataModal(val);
   };
 
   // fetch data on pagination change, sort change, or filter change
@@ -102,21 +109,28 @@ function IuranDetail(props) {
       <Admin>
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <div className="h4 d-flex align-items-center">
-            <FaWallet size={20} className="mx-2" />Detail Iuran Pengguna
+            <FaWallet size={20} className="mx-2" />
+            Detail Iuran Pengguna
           </div>
         </div>
 
         <div className="row">
           <div className="col-md-3 col-sm-12 px-2 py-2 w-10">
-            <img src={`${process.env.PUBLIC_URL}/assets/icon/user.webp`} className="img-fluid" alt="..."/>
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/icon/user.webp`}
+              className="img-fluid"
+              alt="..."
+            />
           </div>
           <div className="col-md-9 col-sm-12">
             <div className="flex-grow-1 p-4">
               <div>
-                <strong>Nama Lengkap:</strong> <span>{data?.fullName ?? ''}</span>
+                <strong>Nama Lengkap:</strong>{' '}
+                <span>{data?.fullName ?? ''}</span>
               </div>
               <div>
-                <strong>Tanggal Lahir:</strong> <span>{data?.birthDate ?? ''}</span>
+                <strong>Tanggal Lahir:</strong>{' '}
+                <span>{data?.birthDate ?? ''}</span>
               </div>
               <div>
                 <strong>Umur:</strong> <span>{data?.age ?? ''}</span>
@@ -127,7 +141,7 @@ function IuranDetail(props) {
 
         <div className="table-responsive">
           <Table
-            columns={coloumn_detail_iuran(handleDataModal)}
+            columns={coloumn_detail_iuran(handleDataModal, handleVerifikasi)}
             rowKey={(record) => record.id}
             dataSource={dataTable}
             pagination={tableParams.pagination}
@@ -144,7 +158,10 @@ function IuranDetail(props) {
           aria-hidden="true"
         >
           <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div className="modal-content border-0 position-relative" style={{overflow: "visible",}}>
+            <div
+              className="modal-content border-0 position-relative"
+              style={{ overflow: 'visible' }}
+            >
               {/* Tombol Close Bulat */}
               <button
                 type="button"
@@ -152,26 +169,28 @@ function IuranDetail(props) {
                 data-bs-dismiss="modal"
                 aria-label="Close"
                 style={{
-                  width: "20px",
-                  padding: "15px",
-                  height: "14px",
-                  top: "-7px",
-                  right: "-10px",
-                  borderRadius: "50%",
-                  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-                  backgroundColor: "#ffc83e",
+                  width: '20px',
+                  padding: '15px',
+                  height: '14px',
+                  top: '-7px',
+                  right: '-10px',
+                  borderRadius: '50%',
+                  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                  backgroundColor: '#ffc83e',
                 }}
-              >x</button>
-              
+              >
+                x
+              </button>
+
               {isLoading && <div className="spinner">Loading...</div>}
-              <img 
-                src={`https://drive.google.com/thumbnail?id=${dataModal?.fileId}&sz=s4000`} 
-                className="img-fluid w-100" 
+              <img
+                src={`https://drive.google.com/thumbnail?id=${dataModal?.fileId}&sz=s4000`}
+                className="img-fluid w-100"
                 alt="Payment Proof"
                 onLoad={() => setIsLoading(false)}
-                onError={(e) => { 
-                  e.target.onerror = null; 
-                  e.target.src = 'assets/images/default-image-payment.webp'; 
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = 'assets/images/default-image-payment.webp';
                   setIsLoading(false);
                 }}
               />
